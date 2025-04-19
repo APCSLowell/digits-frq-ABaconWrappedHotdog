0fcdd0b8758bc7f74043534caadcedf1c762fc65
Alexio <82485835+ABaconWrappedHotdog@users.noreply.github.com>
2025-04-18 23:15:02 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,7 +7,13 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	digitList = new ArrayList<Integer>();
+	int n = num;
+		if(num ==0) digitList.add(0);
+		while(n>0){
+			digitList.add(0,n%10);
+			n=n/10;
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
@@ -17,7 +23,10 @@ public class Digits
 	}
 	
 	public String toString()
-	{
-		return digitList.toString();
+	{	
+		for(int i =0; i<digitList.size()-1;i++){
+			if(digitList.get(i+1)<= digitList.get(i))return false;
+		}
+		return true;
 	}
 }
